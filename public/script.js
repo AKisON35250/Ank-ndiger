423b790f4c0c2af133085b26c2a01932c13e55d9
AKisON35250 <akison089@gmail.com>
2026-03-10 23:11:06 +0100

    Refactor fetch and DOM manipulation in load function

--- public/script.js
+++ public/script.js
@@ -1,41 +1,32 @@
 async function load(){
 
-let data = await fetch("/data").then(r=>r.json())
+const res = await fetch("/data")
+const data = await res.json()
 
-let ch = document.getElementById("channels")
-let rl = document.getElementById("roles")
+const ch = document.getElementById("channels")
+const rl = document.getElementById("roles")
 
-data.channels.forEach(c=>{
+data.channels.forEach(c => {
 
-let o=document.createElement("option")
-o.value=c.id
-o.textContent="# "+c.name
-ch.appendChild(o)
+let opt = document.createElement("option")
+opt.value = c.id
+opt.textContent = "# " + c.name
+ch.appendChild(opt)
 
 })
 
-data.roles.forEach(r=>{
+data.roles.forEach(r => {
 
-let o=document.createElement("option")
-o.value=r.id
-o.textContent="@ "+r.name
-rl.appendChild(o)
+let opt = document.createElement("option")
+opt.value = r.id
+opt.textContent = "@ " + r.name
+rl.appendChild(opt)
 
 })
 
 }
 
 async function send(){
-
-let payload={
-
-channel:channels.value,
-role:roles.value,
-title:title.value,
-message:message.value,
-image:image.value
-
-}
 
 await fetch("/send",{
 
@@ -45,7 +36,14 @@ headers:{
 "Content-Type":"application/json"
 },
 
-body:JSON.stringify(payload)
+body:JSON.stringify({
+
+channel:channels.value,
+role:roles.value,
+title:title.value,
+message:message.value
+
+})
 
 })
 
