8e00dba768a80987393248be3c464e77f8363c5a
AKisON35250 <akison089@gmail.com>
2026-03-10 23:09:55 +0100

    Add ready event listener to bot

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const { Client, GatewayIntentBits } = require("discord.js")
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages
-  ]
+  intents: [GatewayIntentBits.Guilds]
+})
+
+client.once("ready", () => {
+  console.log("Bot ist online")
 })
 
 client.login(process.env.BOT_TOKEN)
